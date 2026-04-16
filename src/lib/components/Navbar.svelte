5d5c0ff2836d8b1b9548917fccc7bf63b5498a7e
Germán <germanrodriguezvillar@bonai.es>
2026-04-16 19:04:16 +0200

    feat: navbar scaffold

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { localeState } from "$lib/state/locale.svelte";
+</script>
+
+<nav>
+	<img alt="Praesidium logo" src="">
+	<ul>
+		<li>
+			{localeState.labels.components.navbar.vault}
+		</li>
+		<li>
+			{localeState.labels.components.navbar.generator}
+		</li>
+		<li>
+			{localeState.labels.components.navbar.about}
+		</li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,28 +2,14 @@
 	// Import Tailwind CSS, delegate to Vite
 	import "../app.css";
 
-    import Sidebar from "$lib/components/Sidebar.svelte";
-    import Main from "$lib/components/Main.svelte";
+	import Navbar from "$lib/components/Navbar.svelte";
 
-	// Get child components (+page.svelte)
+	import { localeState } from "$lib/state/locale.svelte";
+
 	const { children } = $props();
-
-	/* Disable default context menu
-	const preventDefaultContextMenu = (event: MouseEvent) => {
-		event.preventDefault();
-	};
-	
-	window.addEventListener("contextmenu", preventDefaultContextMenu);
-	*/
 </script>
 
-<div class="flex flex-row min-h-screen font-[LeagueSpartan]
-	text-black dark:text-white
-	bg-(--light-scheme-1) dark:bg-(--dark-scheme-1)
-	"
->
-	<!-- 20% horizontal width -->
-	<Sidebar />
-	<!-- 80% horizontal width -->
-	<Main {children} />
+<div>
+	<Navbar></Navbar>
+	{@render children()}
 </div>
