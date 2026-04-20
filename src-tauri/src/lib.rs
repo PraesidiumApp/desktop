7eaa14447e19692b1c693f4e16626f1dc7f043a4
Germán <germanrodriguezvillar@bonai.es>
2026-04-20 16:58:46 +0200

    build: add tauri opener plugin for rust's side

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,20 @@
 use engine::Session;
-use tauri::State;
 use std::sync::Mutex;
+use tauri::State;
 
 // Wrapper struct around engine's Session, Mutexed because of Tauri's requirements
 struct SessionState(Mutex<Option<Session>>);
 
 #[tauri::command]
-async fn new_session(path: String, mut password: String, state: State<'_, SessionState>) -> Result<(), String> {
+async fn new_session(
+    path: String,
+    mut password: String,
+    state: State<'_, SessionState>,
+) -> Result<(), String> {
     let mut lock = state.0.lock().map_err(|e| e.to_string())?;
 
     if lock.is_some() {
-        return Err("A session is already active!".to_string())
+        return Err("A session is already active!".to_string());
     }
 
     *lock = Some(Session::new(path, password.as_mut_str()).map_err(|e| e.to_string())?);
@@ -24,6 +28,7 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![new_session])
         .manage(SessionState(Mutex::new(None)))
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
