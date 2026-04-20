6f855e3f118b44493b5db488019436f47f364d2b
Germán <germanrodriguezvillar@bonai.es>
2026-04-20 13:03:57 +0200

    style: polish dock appareance

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,32 +4,36 @@
 </script>
 
 <nav class="fixed top-5 left-0 w-full h-[15vh] flex justify-center">
-	<div class="h-full p-5 max-w-7xl bg-(--dock-bg) dark:bg-(--dock-bg-dark) border-(--dock-border) dark:border-(--dock-border-dark) backdrop-blur-lg shadow-(--dock-shadow) dark:shadow-(--dock-shadow) rounded-2xl flex gap-5 items-center">
+	<div class="h-full w-[80vw] p-5 bg-(--dock-bg) dark:bg-(--dock-bg-dark) border-(--dock-border) dark:border-(--dock-border-dark) backdrop-blur-lg shadow-(--dock-shadow) dark:shadow-(--dock-shadow-dark) rounded-2xl flex flex-row gap-10 items-center justify-between">
 		<div class="h-full">
-			<img alt="Praesidium logo" src="/imgs/praesidium-{themeState.theme}.png" class="aspect-square h-full w-auto">
+			<a href="/" class="h-full cursor-pointer">
+				<img alt="Praesidium logo" src="/imgs/praesidium-{themeState.theme}.png" class="aspect-square h-full w-auto">
+			</a>
 		</div>
-		<ul class="flex flex-row gap-5 items-center">
-			<li>
-				<a href="/vault">
-					{localeState.labels.components.navbar.vault}
-				</a>
-			</li>
-			<li>
-				<a href="/generator">
-					{localeState.labels.components.navbar.generator}
-				</a>
-			</li>
-			<li>
-				<a href="/about">
-					{localeState.labels.components.navbar.about}
-				</a>
-			</li>
-		</ul>
-		<div class="h-full flex gap-2">
-			<button title="Open Praesidium's GitHub" class="h-full">
+		<div>
+			<ul class="flex flex-row gap-10 font-bold text-4xl">
+				<li>
+					<a href="/">
+						{localeState.labels.components.navbar.vault}
+					</a>
+				</li>
+				<li>
+					<a href="/generator">
+						{localeState.labels.components.navbar.generator}
+					</a>
+				</li>
+				<li>
+					<a href="/about">
+						{localeState.labels.components.navbar.about}
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="h-1/2 flex flex-row gap-5">
+			<button title="Open Praesidium's GitHub" class="h-full cursor-pointer">
 				<img alt="GitHub logo" src="/imgs/github-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 			</button>
-			<button title="Toggle app theme" class="h-full">
+			<button title="Toggle app theme" class="h-full cursor-pointer">
 				<img alt="App theme toggler" src="/imgs/colortheme-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 			</button>
 		</div>
