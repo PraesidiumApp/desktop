c2c21a115348820b1b91586a7aae8169aa09846f
Germán <germanrodriguezvillar@bonai.es>
2026-04-20 16:59:09 +0200

    feat: implement toggle color theme and open github icons

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { localeState } from "$lib/state/locale.svelte";
 	import { themeState } from "$lib/state/theme.svelte";
+	import { openUrl } from "@tauri-apps/plugin-opener";
+
+	function toggleTheme() {
+		themeState.theme === "light" ? themeState.theme = "dark" : themeState.theme = "light";
+	}
+
+	function openGithub() {
+		openUrl("https://github.com/PraesidiumApp");
+	}
 </script>
 
-<nav class="fixed top-5 left-0 w-full h-[15vh] flex justify-center">
-	<div class="h-full w-[80vw] p-5 bg-(--dock-bg) dark:bg-(--dock-bg-dark) border-(--dock-border) dark:border-(--dock-border-dark) backdrop-blur-lg shadow-(--dock-shadow) dark:shadow-(--dock-shadow-dark) rounded-2xl flex flex-row gap-10 items-center justify-between">
+<nav class="fixed top-5 left-0 w-full h-30 flex justify-center">
+	<div class="h-full w-[80vw] p-5 bg-(--dock-bg) dark:bg-(--dock-bg-dark) border-(--dock-border) dark:border-(--dock-border-dark) backdrop-blur-lg shadow-(--dock-shadow) dark:shadow-(--dock-shadow-dark) rounded-2xl border-2 flex flex-row gap-10 items-center justify-between">
 		<div class="h-full">
 			<a href="/" class="h-full cursor-pointer">
 				<img alt="Praesidium logo" src="/imgs/praesidium-{themeState.theme}.png" class="aspect-square h-full w-auto">
@@ -30,10 +39,10 @@
 			</ul>
 		</div>
 		<div class="h-1/2 flex flex-row gap-5">
-			<button title="Open Praesidium's GitHub" class="h-full cursor-pointer">
+			<button title="Open Praesidium's GitHub" class="h-full cursor-pointer" onclick={openGithub}>
 				<img alt="GitHub logo" src="/imgs/github-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 			</button>
-			<button title="Toggle app theme" class="h-full cursor-pointer">
+			<button title="Toggle app theme" class="h-full cursor-pointer" onclick={toggleTheme}>
 				<img alt="App theme toggler" src="/imgs/colortheme-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 			</button>
 		</div>
