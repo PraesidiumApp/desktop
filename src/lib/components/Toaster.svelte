30b40915fa4b5c46c49be02c4d3257619e83cc3b
Germán <germanrodriguezvillar@bonai.es>
2026-04-20 18:44:19 +0200

    style: fix styles for the toaster

--- src/lib/components/Toaster.svelte
+++ src/lib/components/Toaster.svelte
@@ -4,20 +4,17 @@
 	const colors = {
 		info: "bg-blue-500 border-blue-700",
 		warning: "bg-amber-500 border-amber-700",
-		error: "bg-red-500 border-red-700",
+		error: "bg-red-500 border-red-700 dark:bg-zinc-800 dark:border-zinc-700",
 	};
 </script>
 
-<div>
+<div class="fixed bottom-5 right-5 flex flex-col-reverse">
 	{#each toasterState.queue as toast}
 		<div class="{colors[toast.kind]}">
 			<p>
 				{toast.message}
 			</p>
 			<button title="Dismiss" onclick={() => toasterState.remove(toast.id)}>
-				<svg>
-
-				</svg>
 			</button>
 		</div>
 	{/each}
