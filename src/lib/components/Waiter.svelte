31f3bee9a27fb578c11be8ec78ba1958cd72a472
Germán <germanrodriguezvillar@bonai.es>
2026-04-24 00:37:01 +0200

    style: fade in and out for waiter

--- src/lib/components/Waiter.svelte
+++ src/lib/components/Waiter.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { themeState } from "$lib/state/theme.svelte";
     import { waiterState } from "$lib/state/waiter.svelte";
+    import { fade } from "svelte/transition";
 </script>
 
-<div class="fixed top-0 left-0 min-h-screen min-w-screen flex flex-col justify-center items-center bg-(--dock-bg) dark:bg-(--dock-bg-dark) backdrop-blur-sm">
+<div class="fixed top-0 left-0 min-h-screen min-w-screen flex flex-col justify-center items-center bg-(--dock-bg) dark:bg-(--dock-bg-dark) backdrop-blur-sm" in:fade out:fade>
 	<img alt="Loading icon" src="/imgs/loading-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 	<p class="text-3xl">
 		{waiterState.message}
