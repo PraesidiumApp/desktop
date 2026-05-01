a8460db44b41b6ff97fcb7f439f80b069dd3326b
Germán <germanrodriguezvillar@bonai.es>
2026-05-01 13:30:26 +0200

    style: move github icon next to app logo

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -31,10 +31,13 @@
 
 <nav class="fixed top-5 left-0 w-full h-30 flex justify-center">
 	<div class="h-full w-[95vw] max-w-300 p-5 bg-(--dock-bg) dark:bg-(--dock-bg-dark) border-(--dock-border) dark:border-(--dock-border-dark) shadow-(--dock-shadow) dark:shadow-(--dock-shadow-dark) rounded-2xl border-2 flex flex-row gap-10 items-center justify-between backdrop-blur-sm">
-		<div class="h-full">
-			<a href="/" class="h-full cursor-pointer">
+		<div class="flex flex-row h-full items-center gap-5">
+			<a href="/" class="h-full cursor-pointer mb-4">
 				<img alt="Praesidium logo" src="/imgs/praesidium-{themeState.theme}.png" class="aspect-square h-full w-auto">
 			</a>
+			<button title="Open Praesidium's GitHub" class="h-[70%] cursor-pointer" onclick={openGithub}>
+				<img alt="GitHub logo" src="/imgs/github-{themeState.theme}.svg" class="aspect-square h-full w-auto">
+			</button>
 		</div>
 		<div>
 			<ul class="flex flex-row gap-10 font-bold text-4xl">
@@ -56,9 +59,6 @@
 			</ul>
 		</div>
 		<div class="h-[70%] flex flex-row gap-5">
-			<button title="Open Praesidium's GitHub" class="h-full cursor-pointer" onclick={openGithub}>
-				<img alt="GitHub logo" src="/imgs/github-{themeState.theme}.svg" class="aspect-square h-full w-auto">
-			</button>
 			<button title="Toggle app theme" class="h-full cursor-pointer" onclick={toggleTheme}>
 				<img alt="App theme toggler" src="/imgs/colortheme-{themeState.theme}.svg" class="aspect-square h-full w-auto">
 			</button>
